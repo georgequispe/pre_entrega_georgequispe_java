cfade6d317b660353c05fdda2429c5d9ff5ea2f5
george <fuerzayfegeorge@hotmail.com.ar>
2025-09-16 20:41:46 -0300

    revisando

--- src/App.java
+++ src/App.java
@@ -2,22 +2,54 @@ import java.util.Scanner; // Importamos la clase para leer datos
 
 public class App { // Nombre de la clase
     public static void main(String[] args) { // Método principal
-        Scanner scanner = new Scanner(System.in); // Creamos el lector de teclado
+        Scanner scanner = new Scanner(System.in); // Creamos el lector de teclado    
+          
 
-        System.out.print("Ingresa el primer número (a): ");
-        int a = scanner.nextInt(); // Leemos el primer número
+        final int NUMERO_PRODUCTOS = 3;
+        String[] nombresProductos = new String[NUMERO_PRODUCTOS];
+        int[] cantidades = new int[NUMERO_PRODUCTOS];
 
-        System.out.print("Ingresa el segundo número (b): ");
-        int b = scanner.nextInt(); // Leemos el segundo número
+       
+       
+        
+        for (int i = 0; i < NUMERO_PRODUCTOS; i++) {
+            System.out.println("Ingrese el nombre del producto " + (i + 1) + ":");
+            nombresProductos[i] = scanner.next();
+            System.out.print("Ingresa la cantidad de " + nombresProductos[i] + ": ");
+            cantidades[i] = scanner.nextInt();
+        }
 
-        System.out.print("Ingresa el tercer número (c): ");
-        int c = scanner.nextInt(); // Leemos el tercer número
+        System.out.println("Ingrese el nombre del primer producto:");
+        String nombreProducto = scanner.next();       
+        System.out.print("Ingresa la cantidad de " + nombreProducto + ": ");
+        int cantidad1 = scanner.nextInt();
 
-        int suma = a + b + c; // Calculamos la suma
-        scanner.close(); // Cerramos el recurso
+        System.out.println("Ingrese el nombre del segundo producto:");
+        String nombreProducto2 = scanner.next();
+        System.out.print("Ingresa la cantidad de " + nombreProducto2 + ": ");
+        int cantidad2 = scanner.nextInt();
 
+        System.out.println("Ingrese el nombre del tercer producto:");
+        String nombreProducto3 = scanner.next();       
+        System.out.print("Ingresa la cantidad de " + nombreProducto3 + ": ");
+        int cantidad3 = scanner.nextInt();
 
+        System.out.println("Desea comprobar la cantidad de productos? (s/n)");
+        String respuesta = scanner.next();
 
-        System.out.println("La suma de a + b + c es: " + suma); // Mostramos el resultado
+        if (respuesta.equalsIgnoreCase("s")) {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("Cantidad de " + (i == 1 ?
+                 nombreProducto : i == 2 ?
+                 nombreProducto2 : nombreProducto3) + ": " + (i == 1 ? cantidad1 : i == 2 ? cantidad2 : cantidad3));
+            System.out.println("\n--- Resumen de Cantidades ---");
+            for (int i = 0; i < NUMERO_PRODUCTOS; i++) {
+                System.out.println("Cantidad de " + nombresProductos[i] + ": " + cantidades[i]);
+            }
+    } else {
+        } else {
+            System.out.println("Gracias por usar el programa.");
+        }
+        scanner.close(); // Cerramos el lector de teclado
     }
 }
