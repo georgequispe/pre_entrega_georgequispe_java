52cae4f708fdd5bb65626e2b39b66b3a3948e3f7
george <fuerzayfegeorge@hotmail.com.ar>
2025-09-16 21:07:35 -0300

    arregle solo para ingreso de 3 productos y 3 valores

--- src/miproyecto/App.java
+++ src/miproyecto/App.java
@@ -20,31 +20,8 @@ public class App { // Nombre de la clase
             cantidades[i] = scanner.nextInt();
         }
 
-        System.out.println("Ingrese el nombre del primer producto:");
-        String nombreProducto = scanner.next();       
-        System.out.print("Ingresa la cantidad de " + nombreProducto + ": ");
-        int cantidad1 = scanner.nextInt();
-
-        System.out.println("Ingrese el nombre del segundo producto:");
-        String nombreProducto2 = scanner.next();
-        System.out.print("Ingresa la cantidad de " + nombreProducto2 + ": ");
-        int cantidad2 = scanner.nextInt();
-
-        System.out.println("Ingrese el nombre del tercer producto:");
-        String nombreProducto3 = scanner.next();       
-        System.out.print("Ingresa la cantidad de " + nombreProducto3 + ": ");
-        int cantidad3 = scanner.nextInt();
-
-        System.out.println("Desea comprobar la cantidad de productos? (s/n)");
-        String respuesta = scanner.next();
-
-        if (respuesta.equalsIgnoreCase("s")) {
-            for (int i = 1; i <= 3; i++) {
-                System.out.println("Cantidad de " + (i == 1 ?
-                 nombreProducto : i == 2 ?
-                 nombreProducto2 : nombreProducto3) + ": " + (i == 1 ? cantidad1 : i == 2 ? cantidad2 : cantidad3));
-            }
-        }
+       
+        
         System.out.println("\n--- Resumen de Cantidades ---");
         for (int i = 0; i < NUMERO_PRODUCTOS; i++) {
             System.out.println("Cantidad de " + nombresProductos[i] + ": " + cantidades[i]);
